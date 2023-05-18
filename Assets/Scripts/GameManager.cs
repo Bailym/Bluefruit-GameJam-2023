5fa3f1943a8773c9ee57436f25aeb77e7bac49c1
Baily Martin <BM39@hotmail.co.uk>
2023-05-18 19:07:46 +0100

    Add visual warnings for failed mixes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,93 +5,110 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
 
-  public Transform fluidLevelObjectTransform;
-  public FluidLevelHandler fluidLevelHandler;
-  public ColorIncrementButton redColorButtonValues;
-  public ColorIncrementButton greenColorButtonValues;
-  public ColorIncrementButton blueColorButtonValues;
-  public SpriteRenderer targetRGBDisplay;
-  public UIManager uiManager;
-  public ScoreManager scoreManager;
-  private Color targetRGBValue;
-  private float minColorIncrementRate = 0.005f;
-  private float maxColorIncrementRate = 0.05f;
+    public Transform fluidLevelObjectTransform;
+    public FluidLevelHandler fluidLevelHandler;
+    public ColorIncrementButton redColorButtonValues;
+    public ColorIncrementButton greenColorButtonValues;
+    public ColorIncrementButton blueColorButtonValues;
+    public SpriteRenderer targetRGBDisplay;
+    public UIManager uiManager;
+    public ScoreManager scoreManager;
+    private Color targetRGBValue;
+    private float minColorIncrementRate = 0.005f;
+    private float maxColorIncrementRate = 0.05f;
+    private int numberOfWarnings;
+    private int endGameOnNumberOfWarnings = 3;
 
 
-  private void Start()
-  {
-    SetNewTargetRGB();
-  }
-  void SetNewTargetRGB()
-  {
-    uiManager.UpdateRequestText();
-    float randomRedValue = getNewRedTargetValue();
-    float randomGreenValue = getNewGreenTargetValue();
-    float randomBlueValue = getNewBlueTargetValue();
+    private void Start()
+    {
+        SetNewTargetRGB();
+    }
+    void SetNewTargetRGB()
+    {
+        uiManager.UpdateRequestText();
+        float randomRedValue = getNewRedTargetValue();
+        float randomGreenValue = getNewGreenTargetValue();
+        float randomBlueValue = getNewBlueTargetValue();
 
-    targetRGBValue.r = randomRedValue;
-    targetRGBValue.g = randomGreenValue;
-    targetRGBValue.b = randomBlueValue;
-    targetRGBValue.a = 1;
+        targetRGBValue.r = randomRedValue;
+        targetRGBValue.g = randomGreenValue;
+        targetRGBValue.b = randomBlueValue;
+        targetRGBValue.a = 1;
 
-    targetRGBDisplay.color = targetRGBValue;
-  }
+        targetRGBDisplay.color = targetRGBValue;
+    }
 
-  float getNewRedTargetValue()
-  {
-    float redMinValue = redColorButtonValues.GetColorIncrementRate0To255();
-    return Random.Range(redMinValue, 1);
-  }
-  float getNewGreenTargetValue()
-  {
-    float greenMinValue = greenColorButtonValues.GetColorIncrementRate0To255();
-    return Random.Range(greenMinValue, 1);
-  }
-  float getNewBlueTargetValue()
-  {
-    float blueMinValue = blueColorButtonValues.GetColorIncrementRate0To255();
-    return Random.Range(blueMinValue, 1);
-  }
+    float getNewRedTargetValue()
+    {
+        float redMinValue = redColorButtonValues.GetColorIncrementRate0To255();
+        return Random.Range(redMinValue, 1);
+    }
+    float getNewGreenTargetValue()
+    {
+        float greenMinValue = greenColorButtonValues.GetColorIncrementRate0To255();
+        return Random.Range(greenMinValue, 1);
+    }
+    float getNewBlueTargetValue()
+    {
+        float blueMinValue = blueColorButtonValues.GetColorIncrementRate0To255();
+        return Random.Range(blueMinValue, 1);
+    }
 
-  public void adjustFillRateForVariance(float currentIncrementRate)
-  {
-    float currentVariance = CalculateColorVariance();
-    float clampedVariance = Mathf.Clamp(currentVariance, 0f, 1f);
+    public void adjustFillRateForVariance(float currentIncrementRate)
+    {
+        float currentVariance = CalculateColorVariance();
+        float clampedVariance = Mathf.Clamp(currentVariance, 0f, 1f);
 
-    float adjustedIncrementRate = (currentIncrementRate * clampedVariance) + minColorIncrementRate;
-    float clampedAdustedIncrementRate = Mathf.Clamp(adjustedIncrementRate, minColorIncrementRate, maxColorIncrementRate);
+        float adjustedIncrementRate = (currentIncrementRate * clampedVariance) + minColorIncrementRate;
+        float clampedAdustedIncrementRate = Mathf.Clamp(adjustedIncrementRate, minColorIncrementRate, maxColorIncrementRate);
 
-    redColorButtonValues.SetColorIncrementRate(clampedAdustedIncrementRate);
-    greenColorButtonValues.SetColorIncrementRate(clampedAdustedIncrementRate);
-    blueColorButtonValues.SetColorIncrementRate(clampedAdustedIncrementRate);
-  }
+        redColorButtonValues.SetColorIncrementRate(clampedAdustedIncrementRate);
+        greenColorButtonValues.SetColorIncrementRate(clampedAdustedIncrementRate);
+        blueColorButtonValues.SetColorIncrementRate(clampedAdustedIncrementRate);
+    }
 
-  public void FillLineHit()
-  {
-    float finalAccuracy = CalculateAccuracyPercentage();
-    Debug.Log("Accuracy:" + finalAccuracy);
-    SetNewTargetRGB();
-    fluidLevelHandler.ResetFluidLevelPosition();
-    fluidLevelHandler.ResetFluidLevelColor();
-    uiManager.showAccuracyFeedback(finalAccuracy);
-    float scoreFromAccuracy = scoreManager.GetScoreFromAccuracy(finalAccuracy);
-    scoreManager.AddToScore(scoreFromAccuracy);
-  }
+    public void FillLineHit()
+    {
+        float finalAccuracy = CalculateAccuracyPercentage();
+        Debug.Log("Accuracy:" + finalAccuracy);
+        SetNewTargetRGB();
+        fluidLevelHandler.ResetFluidLevelPosition();
+        fluidLevelHandler.ResetFluidLevelColor();
+        uiManager.showAccuracyFeedback(finalAccuracy);
+        float scoreFromAccuracy = scoreManager.GetScoreFromAccuracy(finalAccuracy);
+        scoreManager.AddToScore(scoreFromAccuracy);
+        if (finalAccuracy <= 70f)
+        {
+            AddWarning();
+        }
+    }
 
-  float CalculateColorVariance()
-  {
-    Color currentColor = fluidLevelHandler.GetCurrentColor();
-    float colorVariance = Mathf.Sqrt(Mathf.Pow(currentColor.r - targetRGBValue.r, 2) + Mathf.Pow(currentColor.g - targetRGBValue.g, 2) + Mathf.Pow(currentColor.b - targetRGBValue.b, 2));
+    float CalculateColorVariance()
+    {
+        Color currentColor = fluidLevelHandler.GetCurrentColor();
+        float colorVariance = Mathf.Sqrt(Mathf.Pow(currentColor.r - targetRGBValue.r, 2) + Mathf.Pow(currentColor.g - targetRGBValue.g, 2) + Mathf.Pow(currentColor.b - targetRGBValue.b, 2));
 
-    return colorVariance;
-  }
+        return colorVariance;
+    }
 
-  float CalculateAccuracyPercentage()
-  {
-    // The maximum variance is root 3 since the max value for each color channel is 1
-    float colorVariance = CalculateColorVariance();
-    float variancePercentage = (colorVariance / Mathf.Sqrt(3)) * 100;
-    float accuracyPercentage = 100f - variancePercentage;
-    return accuracyPercentage;
-  }
+    float CalculateAccuracyPercentage()
+    {
+        // The maximum variance is root 3 since the max value for each color channel is 1
+        float colorVariance = CalculateColorVariance();
+        float variancePercentage = (colorVariance / Mathf.Sqrt(3)) * 100;
+        float accuracyPercentage = 100f - variancePercentage;
+        return accuracyPercentage;
+    }
+
+    void AddWarning()
+    {
+        ++numberOfWarnings;
+        uiManager.DisplayWarningIcons(numberOfWarnings);
+
+        if (numberOfWarnings == endGameOnNumberOfWarnings)
+        {
+            Debug.Log("GAME OVER");
+        }
+    }
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -6,75 +6,82 @@ using TMPro;
 
 public class UIManager : MonoBehaviour
 {
-  public Image FeedbackPanelImage;
-  public FeedbackSprites FeedBackSpritesGrades;
-  public ParticleSystem FeedBackParticleSystem;
-  public TextMeshProUGUI requestText;
-  private List<string> requestTextOptions;
+    public Image FeedbackPanelImage;
+    public FeedbackSprites FeedBackSpritesGrades;
+    public ParticleSystem FeedBackParticleSystem;
+    public TextMeshProUGUI requestText;
+    public List<SpriteRenderer> warningIcons;
+    private List<string> requestTextOptions;
 
-  private void Start()
-  {
-    requestTextOptions = new List<string>();
-    requestTextOptions.Add("Im sure this is the one we're looking for!");
-    requestTextOptions.Add("This one's going to work!");
-    requestTextOptions.Add("We're getting close! Try this!");
-    requestTextOptions.Add("If my calculations are correct, this is what we need to make.");
-    requestTextOptions.Add("Make an elixir this colour");
-    requestTextOptions.Add("Im certain of this one...");
-  }
+    private void Start()
+    {
+        requestTextOptions = new List<string>();
+        requestTextOptions.Add("Im sure this is the one we're looking for!");
+        requestTextOptions.Add("This one's going to work!");
+        requestTextOptions.Add("We're getting close! Try this!");
+        requestTextOptions.Add("If my calculations are correct, this is what we need to make.");
+        requestTextOptions.Add("Make an elixir this colour");
+        requestTextOptions.Add("Im certain of this one...");
+    }
 
-  public void UpdateRequestText()
-  {
-    int numberOfOptions = requestTextOptions.Count - 1;
-    int randomOptionIndex = Random.Range(0, numberOfOptions);
+    public void UpdateRequestText()
+    {
+        int numberOfOptions = requestTextOptions.Count - 1;
+        int randomOptionIndex = Random.Range(0, numberOfOptions);
 
-    requestText.text = requestTextOptions[randomOptionIndex].ToString();
-  }
-  public void showAccuracyFeedback(float accuracyPercentage)
-  {
-    if (accuracyPercentage > 90f)
-    {
-      FeedbackPanelImage.sprite = FeedBackSpritesGrades.AGradeSprite;
+        requestText.text = requestTextOptions[randomOptionIndex].ToString();
     }
-    else if (accuracyPercentage > 85f)
+    public void showAccuracyFeedback(float accuracyPercentage)
     {
-      FeedbackPanelImage.sprite = FeedBackSpritesGrades.BGradeSprite;
+        if (accuracyPercentage > 90f)
+        {
+            FeedbackPanelImage.sprite = FeedBackSpritesGrades.AGradeSprite;
+        }
+        else if (accuracyPercentage > 85f)
+        {
+            FeedbackPanelImage.sprite = FeedBackSpritesGrades.BGradeSprite;
+        }
+        else if (accuracyPercentage > 80f)
+        {
+            FeedbackPanelImage.sprite = FeedBackSpritesGrades.CGradeSprite;
+        }
+        else if (accuracyPercentage > 75f)
+        {
+            FeedbackPanelImage.sprite = FeedBackSpritesGrades.DGradeSprite;
+        }
+        else if (accuracyPercentage > 70f)
+        {
+            FeedbackPanelImage.sprite = FeedBackSpritesGrades.EGradeSprite;
+        }
+        else
+        {
+            FeedbackPanelImage.sprite = FeedBackSpritesGrades.FGradeSprite;
+        }
+        ShowFeedbackIcon();
+        PlayFeedbackParticles();
     }
-    else if (accuracyPercentage > 80f)
-    {
-      FeedbackPanelImage.sprite = FeedBackSpritesGrades.CGradeSprite;
-    }
-    else if (accuracyPercentage > 75f)
-    {
-      FeedbackPanelImage.sprite = FeedBackSpritesGrades.DGradeSprite;
-    }
-    else if (accuracyPercentage > 70f)
-    {
-      FeedbackPanelImage.sprite = FeedBackSpritesGrades.EGradeSprite;
-    }
-    else
-    {
-      FeedbackPanelImage.sprite = FeedBackSpritesGrades.FGradeSprite;
-    }
-    ShowFeedbackIcon();
-    PlayFeedbackParticles();
-  }
 
-  public void ShowFeedbackIcon()
-  {
-    FeedbackPanelImage.enabled = true;
-    Invoke("HideFeedbackIcon", 1f);
-  }
+    public void ShowFeedbackIcon()
+    {
+        FeedbackPanelImage.enabled = true;
+        Invoke("HideFeedbackIcon", 1f);
+    }
 
-  void HideFeedbackIcon()
-  {
-    FeedbackPanelImage.enabled = false;
-  }
+    void HideFeedbackIcon()
+    {
+        FeedbackPanelImage.enabled = false;
+    }
 
-  public void PlayFeedbackParticles()
-  {
-    FeedBackParticleSystem.Play();
-  }
+    public void PlayFeedbackParticles()
+    {
+        FeedBackParticleSystem.Play();
+    }
+
+    public void DisplayWarningIcons(int numberOfWarnings)
+    {
+        int warningSpriteIndex = numberOfWarnings - 1;
+        warningIcons[warningSpriteIndex].enabled = true;
+    }
 }
 
 
